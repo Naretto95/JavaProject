44d851f6fac0b761f2bb4bbd5a11c9fadb7edc17
CorentinBrillant <65302240+CorentinBrillant@users.noreply.github.com>
2020-05-20 23:52:05 +0200

    Add files via upload

--- Jeu/src/controle/ControleJoueur.java
+++ Jeu/src/controle/ControleJoueur.java
@@ -19,8 +19,8 @@ public class ControleJoueur extends ControleEntite implements EventHandler<KeyEv
 	
 	
 	private Joueur joueur;
-	private int distMinBordEcranX=20;
-	private int distMinBordEcranY=20;	
+	private int distMinBordEcranX=50;
+	private int distMinBordEcranY=50;	
 	
 
 	public ControleJoueur(String feuilleDeSpriteEntite,Carte carte, Joueur joueur, GraphicsContext gc,int hauteurPixelEntite,int largeurPixelEntite){
@@ -84,117 +84,25 @@ public class ControleJoueur extends ControleEntite implements EventHandler<KeyEv
 
 		switch (kc) {
 		case UP:
-			//si le perso n'atteint pas le bord supérieur de l'écran avec le prochain mouvement demandé
-			if (this.getPositionYPixel()-vitesse-this.distMinBordEcranY>0) {
-				
-				//si le perso veut monter et qu'il est décalé sur la droite de sa case. Il faut vérifier que la case au dessus et celle au dessus à droite soient vide toutes les deux
-				if (this.carte.getLargeurCasePixel()*(this.joueur.getPositionX()+1)-this.carte.getFenetreEcran().getPosXPixelEcran()<this.getPositionXPixel()+this.carte.getLargeurCasePixel()-this.paddingX) {
-					if( this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()+1).getContenu()==Case.VIDE) {
-						this.changerPositionPixel(0, -vitesse);
-					}
-				}
-				
-				//si le perso veut monter et qu'il est décalé sur la gauche de sa case. Il faut vérifier que la case au dessus et celle au dessus à gauche soient vide toutes les deux
-				else if (this.carte.getLargeurCasePixel()*(this.joueur.getPositionX())-this.carte.getFenetreEcran().getPosXPixelEcran()>this.getPositionXPixel()+this.paddingX) {
-					if( this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()-1).getContenu()==Case.VIDE) {
-						this.changerPositionPixel(0, -vitesse);
-					}
-				}
-				//si le perso veut monter et qu'il est exactement dans l'axe verticale qui passe par sa case, on regarde si la case au dessus est vide pour avancer
-				else {
-					if (this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()).getContenu()==Case.VIDE)	{
-						this.changerPositionPixel(0, -vitesse);
-					}
-				}
-			}
-			//si le milieu du carré mobile du joueur a dépassé celui le bord de la case du dessus, alors il est dans cette nouvelle case
+			if (!detecteCollisionsMouvement(0,-vitesse)) {this.changerPositionPixel(0, -vitesse);}
 			if(this.carte.getFenetreEcran().getPosYPixelEcran()+this.getPositionYPixel()+this.carte.getHauteurCasePixel()/2<(this.joueur.getPositionY())*this.carte.getHauteurCasePixel()){
 				this.carte.mettreEntite(this.joueur, this.joueur.getPositionY()-1, this.joueur.getPositionX());
 			}
 			break;
 		case DOWN:
-			if (this.getPositionYPixel()+vitesse+this.distMinBordEcranY<this.carte.getFenetreEcran().getLargeurPixelEcran()) {
-				if (this.carte.getLargeurCasePixel()*(this.joueur.getPositionX()+1)-this.carte.getFenetreEcran().getPosXPixelEcran()<this.getPositionXPixel()+this.carte.getLargeurCasePixel()-this.paddingX) {
-					if( this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()+1).getContenu()==Case.VIDE) {
-						this.changerPositionPixel(0, vitesse);
-					}
-				}
-				else if (this.carte.getLargeurCasePixel()*(this.joueur.getPositionX())-this.carte.getFenetreEcran().getPosXPixelEcran()>this.getPositionXPixel()+this.paddingX) {
-					if( this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()-1).getContenu()==Case.VIDE) {
-						this.changerPositionPixel(0, vitesse);
-					}
-				}
-				else {
-					if (this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()).getContenu()==Case.VIDE)	{
-						this.changerPositionPixel(0, vitesse);
-					}
-				}
-			}
+			if (!detecteCollisionsMouvement(0,vitesse)) {this.changerPositionPixel(0, vitesse);}
 			if(this.carte.getFenetreEcran().getPosYPixelEcran()+this.getPositionYPixel()+this.carte.getHauteurCasePixel()/2>(this.joueur.getPositionY()+1)*this.carte.getHauteurCasePixel()){
 				this.carte.mettreEntite(this.joueur, this.joueur.getPositionY()+1, this.joueur.getPositionX());
 			}
 			break;
 		case LEFT:
-			//si on est encore à droite du bord de l'écran
-			if (this.getPositionXPixel()-vitesse-this.distMinBordEcranY>0) {
-				//si on ne rentre pas en collision avec la case de gauche
-				if (this.getPositionXPixel()+this.carte.getFenetreEcran().getPosXPixelEcran()-vitesse-(this.joueur.getPositionX()*this.carte.getLargeurCasePixel())>0)	{
-					this.changerPositionPixel(-vitesse, 0);
-				}else {
-					//si le personnage veut aller à gauche et qu'il est décalé sur le haut de sa case, on vérifie que la case de gauche et celle au dessus à gauche sont toutes les deux vides
-					if (this.joueur.getPositionY()*this.carte.getHauteurCasePixel()-this.carte.getFenetreEcran().getPosYPixelEcran()>this.getPositionYPixel()+this.paddingY) {
-						System.out.println("coucou1");
-						if( this.carte.getCase(this.joueur.getPositionY(), this.joueur.getPositionX()-1).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()-1).getContenu()==Case.VIDE) {
-							this.changerPositionPixel(-vitesse, 0);
-						}
-					}
-					//si le personnage veut aller à gauche et qu'il est décalé sur le bas de sa case, on vérifie que la case de gauche et celle en dessous à gauche sont toutes les deux vides
-					else if ((this.joueur.getPositionY()+1)*this.carte.getHauteurCasePixel()-this.carte.getFenetreEcran().getPosYPixelEcran()>this.getPositionYPixel()+this.carte.getHauteurCasePixel()+this.paddingY ){
-						System.out.println("coucou2");
-						if( this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()-1).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY(), this.joueur.getPositionX()-1).getContenu()==Case.VIDE) {
-							this.changerPositionPixel(-vitesse, 0);
-						}
-					}
-					//sinon le perso est aligné dans sa case et on regarde simplement si la case de gauche est vide
-					else {
-						System.out.println("coucou3");
-						if (  this.carte.getCase(this.joueur.getPositionY(), this.joueur.getPositionX()-1).getContenu()==Case.VIDE){
-								this.changerPositionPixel(-vitesse, 0);
-						}
-					}
-				}
-			}
+			if (!detecteCollisionsMouvement(-vitesse,0)) {this.changerPositionPixel(-vitesse, 0);}
 			if(this.carte.getFenetreEcran().getPosXPixelEcran()+this.getPositionXPixel()+this.carte.getLargeurCasePixel()/2<(this.joueur.getPositionX())*this.carte.getLargeurCasePixel()){
 				this.carte.mettreEntite(this.joueur, this.joueur.getPositionY(), this.joueur.getPositionX()-1);
 			}
 			break;
 		case RIGHT:
-			//si on est encore à gauche du bord de l'écran
-			if (this.getPositionXPixel()+vitesse+this.distMinBordEcranY<this.carte.getFenetreEcran().getLargeurPixelEcran()) {
-				//sin on ne rentre pas en collision avec la case de droite
-				if (this.getPositionXPixel()+this.carte.getFenetreEcran().getPosXPixelEcran()+this.carte.getLargeurCasePixel()+vitesse-((this.joueur.getPositionX()+1)*this.carte.getLargeurCasePixel())<1)	{
-					this.changerPositionPixel(vitesse, 0);
-				} else {
-					//si le personnage veut aller à droite et qu'il est décalé sur le haut de sa case, on vérifie que la case de gauche et celle au dessus à droite sont toutes les deux vides
-					if (this.joueur.getPositionY()*this.carte.getHauteurCasePixel()-this.carte.getFenetreEcran().getPosYPixelEcran()>this.getPositionYPixel()+this.paddingY) {
-						if( this.carte.getCase(this.joueur.getPositionY(), this.joueur.getPositionX()+1).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY()-1, this.joueur.getPositionX()+1).getContenu()==Case.VIDE) {
-							this.changerPositionPixel(vitesse, 0);
-						}
-					}
-					//si le personnage veut aller à droite et qu'il est décalé sur le bas de sa case, on vérifie que la case de gauche et celle en dessous à droite sont toutes les deux vides
-					else if ((this.joueur.getPositionY()+1)*this.carte.getHauteurCasePixel()-this.carte.getFenetreEcran().getPosYPixelEcran()>this.getPositionYPixel()+this.carte.getHauteurCasePixel()+this.paddingY ){
-						if( this.carte.getCase(this.joueur.getPositionY()+1, this.joueur.getPositionX()+1).getContenu()==Case.VIDE && this.carte.getCase(this.joueur.getPositionY(), this.joueur.getPositionX()+1).getContenu()==Case.VIDE) {
-							this.changerPositionPixel(vitesse, 0);
-						}
-					}
-					//sinon le perso est aligné dans sa case et on regarde simplement si la case de droite est vide
-					else {
-						if (this.carte.getCase(this.joueur.getPositionY(), this.joueur.getPositionX()+1).getContenu()==Case.VIDE)	{
-							this.changerPositionPixel(vitesse, 0);
-						}
-					}
-				}
-			}
+			if (!detecteCollisionsMouvement(vitesse,0)) {this.changerPositionPixel(vitesse, 0);}
 			if(this.carte.getFenetreEcran().getPosXPixelEcran()+this.getPositionXPixel()+this.carte.getLargeurCasePixel()/2>(this.joueur.getPositionX()+1)*this.carte.getLargeurCasePixel()){
 				this.carte.mettreEntite(this.joueur, this.joueur.getPositionY(), this.joueur.getPositionX()+1);
 			}
@@ -212,25 +120,47 @@ public class ControleJoueur extends ControleEntite implements EventHandler<KeyEv
 		switch(keycode) {
 		//pour le joueur qui se déplace dans plusieurs directions il faut adapter l'image au sens du mouvement
 		case RIGHT:
-			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,925,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel(),this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel());
-			gc.fillRect(this.getPositionXPixel(), this.getPositionYPixel(), 50, 50);
+			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,925,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel()+this.carte.getHauteurCasePixel()-this.hauteurPixelEntite,this.largeurPixelEntite,this.hauteurPixelEntite);
 			break;
 		case LEFT:
-			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,660,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel(),this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel());
-			gc.fillRect(this.getPositionXPixel(), this.getPositionYPixel(), 50, 50);
+			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,660,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel()+this.carte.getHauteurCasePixel()-this.hauteurPixelEntite,this.largeurPixelEntite,this.hauteurPixelEntite);
 			break;
 		case UP:
-			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,785,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel(),this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel());
-			gc.fillRect(this.getPositionXPixel(), this.getPositionYPixel(), 50, 50);
+			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,785,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel()+this.carte.getHauteurCasePixel()-this.hauteurPixelEntite,this.largeurPixelEntite,this.hauteurPixelEntite);
 			break;
 		case DOWN:
-			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,530,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel(),this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel());
-			gc.fillRect(this.getPositionXPixel(), this.getPositionYPixel(), 50, 50);
+			gc.drawImage(this.feuilleDeSpriteEntite,indiceSprite*this.largeurPixelEntite,530,this.largeurPixelEntite,this.hauteurPixelEntite,this.getPositionXPixel(),this.getPositionYPixel()+this.carte.getHauteurCasePixel()-this.hauteurPixelEntite,this.largeurPixelEntite,this.hauteurPixelEntite);
 			break;
 		default:
 			break;
 		}
 
+	}
+	
+	public boolean detecteCollision(int rect1x,int rect1y, int rect1w, int rect1h,int rect2x,int rect2y, int rect2w, int rect2h) {
+		return ((rect1x +paddingX< rect2x + rect2w) && (rect1x-paddingX + rect1w > rect2x) && ( rect1y+paddingY < rect2y + rect2h) && ( rect1h + rect1y > rect2y));
+	}
+	public boolean detecteCollisionCaseMouvement(int i, int j,int deltaX, int deltaY) {
+		if(i>=0 && j>=0 && i<this.carte.getImagesCasesCarte().size() && j<this.carte.getImagesCasesCarte().get(i).size()) {
+			return detecteCollision(this.getPositionXPixel()+deltaX,this.getPositionYPixel()+deltaY,this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel(),j*this.carte.getLargeurCasePixel()-this.carte.getFenetreEcran().getPosXPixelEcran(),i*this.carte.getHauteurCasePixel()-this.carte.getFenetreEcran().getPosYPixelEcran(),this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel()) && (this.carte.getCase(i, j).getContenu()!=Case.VIDE);
+		}
+		return true;
+	}
+	public boolean detecteCollisionsMouvement(int deltaX,int deltaY) {
+		//soit deltax est nul soit deltay est nul
+		if (deltaX>0) {
+			return (detecteCollisionCaseMouvement(this.joueur.getPositionY()-1,this.joueur.getPositionX()+1,deltaX,deltaY) ||detecteCollisionCaseMouvement(this.joueur.getPositionY(),this.joueur.getPositionX()+1,deltaX,deltaY)||detecteCollisionCaseMouvement(this.joueur.getPositionY()+1,this.joueur.getPositionX()+1,deltaX,deltaY));
+		}
+		if (deltaX<0) {
+			return (detecteCollisionCaseMouvement(this.joueur.getPositionY()-1,this.joueur.getPositionX()-1,deltaX,deltaY) ||detecteCollisionCaseMouvement(this.joueur.getPositionY(),this.joueur.getPositionX()-1,deltaX,deltaY)||detecteCollisionCaseMouvement(this.joueur.getPositionY()+1,this.joueur.getPositionX()-1,deltaX,deltaY));
+		}
+		if (deltaY>0) {
+			return (detecteCollisionCaseMouvement(this.joueur.getPositionY()+1,this.joueur.getPositionX()-1,deltaX,deltaY) ||detecteCollisionCaseMouvement(this.joueur.getPositionY()+1,this.joueur.getPositionX(),deltaX,deltaY)||detecteCollisionCaseMouvement(this.joueur.getPositionY()+1,this.joueur.getPositionX()+1,deltaX,deltaY));
+		}
+		if (deltaY<0) {
+			return (detecteCollisionCaseMouvement(this.joueur.getPositionY()-1,this.joueur.getPositionX()-1,deltaX,deltaY) ||detecteCollisionCaseMouvement(this.joueur.getPositionY()-1,this.joueur.getPositionX(),deltaX,deltaY)||detecteCollisionCaseMouvement(this.joueur.getPositionY()-1,this.joueur.getPositionX()+1,deltaX,deltaY));
+		}
+		return true;
 	}
 	
 	public void changerPositionPixel(int deltaXPixel, int deltaYPixel) {
