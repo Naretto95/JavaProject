a203701504ec38f27dfa336e888e1763cee79926
CorentinBrillant <65302240+CorentinBrillant@users.noreply.github.com>
2020-05-25 15:47:19 +0200

    drfg

--- Jeu/src/controle/ControleCarte.java
+++ Jeu/src/controle/ControleCarte.java
@@ -7,13 +7,15 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public class ControleCarte {
-	 
+	
+	public static Image staff; 
 	private Carte carte;
 	private GraphicsContext gc;
 	
 	public ControleCarte(Carte carte, GraphicsContext gc) {
 		this.carte = carte;
 		this.gc = gc;
+		ControleCarte.staff= new Image("file:imagesitems/staff.png",this.carte.getLargeurCasePixel(),this.carte.getHauteurCasePixel(),false,true);	
 	}
 	
 
@@ -24,6 +26,7 @@ public class ControleCarte {
 		for (int i = 0 ; i < imagesCasesCarte.size();i++) {
 			for (int j = 0 ; j < imagesCasesCarte.get(i).size();j++) {
 				gc.drawImage(imagesCasesCarte.get(i).get(j), this.carte.getHauteurCasePixel()*j, this.carte.getLargeurCasePixel()*i);
+				if (this.carte.getCase(i, j).hasStaff()) {gc.drawImage(ControleCarte.staff, this.carte.getHauteurCasePixel()*j, this.carte.getLargeurCasePixel()*i);}
 			}
 		}
 	}
