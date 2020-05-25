ee740d1265625fe1d8197d8a6db35cc19548a47e
laatarsiil <65302331+laatarsiil@users.noreply.github.com>
2020-05-25 17:11:43 +0200

    Update Partie.java

--- Jeu/src/Jeu/Partie.java
+++ Jeu/src/Jeu/Partie.java
@@ -1,46 +1,81 @@
 package jeu;
 import java.util.HashMap;
-
+import java.util.Timer;
 public class Partie {
-	private int Duree;
+	private Timer Duree;
 	private TypeIssus Issus;
-	public HashMap<Coordonnee,Case> Carte;
+	public Carte Carte;
 	
+	public boolean paused;
+	public boolean win;
+	
+	//........................................................................
 	
 	public Partie() {
+		this.Duree = new Timer();
+		this.Carte= new Carte(null, null);
+		Joueur heros = new Joueur("Lilian",1,0,0);
+    	Ennemi monstre = new Ennemi(1,1,0,Categorie.Normal,Race.Humain);
+    	monstre.setExperienceMonstre(110);
+    	heros.Utiliser(monstre);
+    	heros.setEtat(EtatEntité.Vivant);
+    	heros.setExperience(0);
+    	paused = false;
+    	
+    	while (!gameOver() && !paused && !win) {
+    		jouer();
+    	}
+    	if(gameOver()) {System.out.println("GAME OVER");} // or show a "gameover" screen
+    	else if (paused) {pause();}
+    	else if (win) {System.out.println("CONGRATS");} // or show "congrats" screen
+	}
 		
+	//..........................................................................
+	
+	public boolean gameOver() {
+		if (Joueur.getEtat()==EtatEntité.Mort) {return true;}
+		else {return false;}
 	}
 	
-	public void Gameover() {
-		
-	}
 	
 	public void pause() {
-		
+		this.Duree.cancel();
+		//show pause screen
 	}
+		
+	
 	
 	public void save() {
 		
 	}
-	
 	public void exit() {
-		
+		if (/*the player presses the exit button*/) {System.out.println("you ended the game")
+			System.exit(0);}
 	}
 	
+	
 	public void recommencer() {
-		
+		while (!(gameOver()) || !win )
+		 {
+		      if (!paused)
+		      { jouer();}
+		 }
 	}
 	
 	public void jouer() {
+		while (!win && !gameOver()) {
+			//play and update the map
+		}
 		
 	}
 	
 	
-	public int getDuree() {
+	//..................................................................................
+	public Timer getDuree() {
 		return Duree;
 	}
 	
-	public void setDuree(int duree) {
+	public void setDuree(Timer duree) {
 		Duree = duree;
 	}
 	
@@ -52,15 +87,14 @@ public class Partie {
 		Issus = issus;
 	}
 	
-	 public void setValue(HashMap<Coordonnee,Case> carte)
+	 public void setValue(Carte carte)
      {
        Carte = carte;
      }
-    public HashMap<Coordonnee,Case> getCarte()
+    public Carte getCarte()
      {
       return Carte;
      }
-	
 
 
 }
