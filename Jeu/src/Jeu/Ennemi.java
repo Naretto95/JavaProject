e288677b6a65e864b988108123512c05c3af4c29
Naretto95 <narettolilian@hotmail.fr>
2020-05-30 02:41:55 +0200

    bug arme

--- Jeu/src/Jeu/Ennemi.java
+++ Jeu/src/Jeu/Ennemi.java
@@ -26,31 +26,31 @@ public class Ennemi extends Entité implements Serializable {
 		}
 		switch (this.getRace()) {
 		case Orc:
-			this.setEnMain(new Arme(TypeArme.EpéeLongue,this.getNiveau()));
 			this.getInventaireArme().add(new Arme(TypeArme.EpéeLongue,this.getNiveau()));
+			this.ChangerItem(new Arme(TypeArme.EpéeLongue,this.getNiveau()));
 			this.getInventairePotion().add(new Potion(Effet.Poison,this.getNiveau()));
 			this.getInventaireRessource().put(new Ressource(TypeRessource.Fer),1);
 			this.getInventaireRessource().put(new Ressource(TypeRessource.Or),1);
 			break;
 			
 		case Humain:
-			this.setEnMain(new Arme(TypeArme.EpéeCourte,this.getNiveau()));
 			this.getInventaireArme().add(new Arme(TypeArme.EpéeCourte,this.getNiveau()));
+			this.ChangerItem(new Arme(TypeArme.EpéeCourte,this.getNiveau()));
 			this.getInventaireRessource().put(new Ressource(TypeRessource.Bois),2);
 			this.getInventairePotion().add(new Potion(Effet.GainDeVie,this.getNiveau()));
 			break;
 			
 		case Dragon:
-			this.setEnMain(new Arme(TypeArme.EpéeLongue,this.getNiveau()));
 			this.getInventaireArme().add(new Arme(TypeArme.EpéeLongue,this.getNiveau()));
+			this.ChangerItem(new Arme(TypeArme.EpéeLongue,this.getNiveau()));
 			this.getInventairePotion().add(new Potion(Effet.GainDegats,this.getNiveau()));
 			this.getInventaireRessource().put(new Ressource(TypeRessource.Or),2);
 			
 			break;
 			
 		case Nain:
-			this.setEnMain(new Arme(TypeArme.Arc,this.getNiveau()));
 			this.getInventaireArme().add(new Arme(TypeArme.Arc,this.getNiveau()));
+			this.ChangerItem(new Arme(TypeArme.Arc,this.getNiveau()));
 			this.getInventairePotion().add(new Potion(Effet.Etourdissement,this.getNiveau()));
 			this.getInventaireRessource().put(new Ressource(TypeRessource.Fer),2);
 			
--- Jeu/src/Jeu/Main.java
+++ Jeu/src/Jeu/Main.java
@@ -8,23 +8,31 @@ public class Main {
 public static void main(String[] args) {
 	Joueur heros = new Joueur("Lilian",1,0,0);
 	Ennemi monstre = new Ennemi(1,1,0,Categorie.Normal,Race.Humain);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	heros.Utiliser(monstre);
-	System.out.println(monstre.getEtat());
+	monstre.getInventaireArme().add(new Arme(TypeArme.Arc,1));
+	monstre.getInventaireArme().add(new Arme(TypeArme.Arc,1));
 	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
 	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
 	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
 	monstre.Utiliser(heros);
-	System.out.println(monstre.getEtat());
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
+	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
+	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
+	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
+	monstre.Utiliser(heros);
+	System.out.println(monstre.getEnMain());
+	System.out.println(monstre.getInventaireArme());
 }
 }
